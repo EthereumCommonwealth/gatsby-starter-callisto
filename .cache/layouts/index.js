2ad1dfca5ce47776c40b54c7222b383b03cdd4d0
Carlos Sampol <sampol.90@gmail.com>
2018-03-06 20:51:36 -0500

    Deleting cache folder and adding it to .gitignore

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/carlossampol/Proyectos/gatsby-starter-callisto/src/layouts/index.js"
-  import data from "/Users/carlossampol/Proyectos/gatsby-starter-callisto/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
